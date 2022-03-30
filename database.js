25040e4afe3adca37a4cc5d1a5b7aed666624a63
LuilliAWSimp <luilliielsexy@gmail.com>
2022-03-29 22:47:40 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-    host : "localhost",
-    user : "root",
-    password : "",
+    host : "us-cdbr-east-05.cleardb.net",
+    user : "ba8c0aaf211cf9",
+    password : "1a8db90f",
     port : 3306,
-    database: "autosmodayrock",
+    database: "heroku_fe6d95d20395c04",
     secret_key: "Moderatto"
 }
 exports.config = config;
